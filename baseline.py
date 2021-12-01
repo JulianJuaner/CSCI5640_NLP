a0dd1142ef1f242036a17d62144855b2541429d7
ZHANG Yuechen <julianjuaner@gmail.com>
2021-12-01 16:42:10 +0800

    add baseline

---
+++ baseline.py
@@ -0,0 +1,40 @@
+from flair.datasets import CONLL_03
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+# 1. get the corpus
+corpus = CONLL_03()
+print(corpus)
+
+# 2. what label do we want to predict?
+label_type = 'ner'
+
+# 3. make the label dictionary from the corpus
+label_dict = corpus.make_label_dictionary(label_type=label_type)
+print(label_dict)
+
+# 4. initialize embedding stack with Flair and GloVe
+embedding_types = [
+    WordEmbeddings('glove'),
+    FlairEmbeddings('news-forward'),
+    FlairEmbeddings('news-backward'),
+]
+
+embeddings = StackedEmbeddings(embeddings=embedding_types)
+
+# 5. initialize sequence tagger
+tagger = SequenceTagger(hidden_size=256,
+                        embeddings=embeddings,
+                        tag_dictionary=label_dict,
+                        tag_type=label_type,
+                        use_crf=True)
+
+# 6. initialize trainer
+trainer = ModelTrainer(tagger, corpus)
+
+# 7. start training
+trainer.train('resources/taggers/sota-ner-flair',
+              learning_rate=0.1,
+              mini_batch_size=32,
+              max_epochs=150)
